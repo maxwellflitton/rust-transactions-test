a8db65a877d6d6430bd7174f2eac42ac602e70bb
Maxwell Flitton <maxwellflitton@Maxwells-MacBook-Pro.local>
2022-03-07 01:19:48 +0000

    sorting out README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # rust-transactions-test
+This repo houses a simple transaction engine. 
+
+## Assumptions 
+It's assumed that when the account has been locked 
+no more transactions go through the account and there 
+is no further effect on the account. 
+
+## Running the application 
+The application can be run with the following command:
+```commandline
+cargo run -- transactions.csv > outcome.csv
+```
+Where ```transactions.csv``` is the file that is 
+being loaded, and the ```outcome.csv``` is where 
+the result state of the accounts after processing the 
+transactions. 
--- transactions/src/data_access_layer/schema.rs
+++ transactions/src/data_access_layer/schema.rs
@@ -5,6 +5,13 @@ use super::super::transactions::transaction::Transaction;
 use super::super::accounts::account::Account;
 
 
+/// This struct is responsible for Deserialising transactions from the CSV file. 
+/// 
+/// # Attributes 
+/// * transaction_type (String): the type of transaction (can be called "type" in the CSV)
+/// * client (i32): the ID of the user who is making the transaction 
+/// * tx (i32): the ID of the transaction 
+/// * amount (Option<f32>): the amount of the transaction
 #[derive(Debug, Deserialize)]
 pub struct TransactionSchema {
     #[serde(alias = "type")]
@@ -16,6 +23,10 @@ pub struct TransactionSchema {
 
 impl TransactionSchema {
 
+    /// Concerts the struct into a Transaction struct. 
+    /// 
+    /// # returns 
+    /// * (Transaction): the transaction struct fit for processing
     pub fn convert_to_transaction(self) -> Transaction {
         let transaction_type = TransactionType::new(self.transaction_type);
         return Transaction{transaction_type, client: self.client, tx: self.tx, amount: self.amount}
@@ -23,6 +34,14 @@ impl TransactionSchema {
 }
 
 
+/// This struct is responsible for serialising account data to be written to a CSV file. 
+/// 
+/// # Attributes 
+/// * client (i32): the ID of the cient and thus the account 
+/// * available (f32): the amount of funds available 
+/// * held (f32): the amount of funds held 
+/// * total (f32): the total amount of funds 
+/// * locked (bool): if the account is locked or not 
 #[derive(Debug, Serialize)]
 pub struct AccountSchema {
     pub client: i32,
@@ -34,6 +53,13 @@ pub struct AccountSchema {
 
 impl AccountSchema {
 
+    /// Gets data from account that has been processed to be ready to be written. 
+    /// 
+    /// # Arguments 
+    /// * account (Account): the account to be serialised
+    /// 
+    /// # Returns 
+    /// (AccountSchema): the schema to be written to CSV 
     pub fn convert_from_account(account: Account) -> AccountSchema {
         return AccountSchema{client: account.id, available: account.amount_available, held: account.amount_held, total: account.total, locked: account.locked}
     }
